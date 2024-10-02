7845b7bb7cf236272353d110754bd098b01f1b0e
James Esguerra <jamesesguerra025@gmail.com>
2024-10-02 23:04:20 +0800

    add initial services and middleware

--- kitchenette-server/Program.cs
+++ kitchenette-server/Program.cs
@@ -1,5 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddProblemDetails();
+
 var app = builder.Build();
 
-app.UseWelcomePage();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseStatusCodePages();
+
 app.Run();
