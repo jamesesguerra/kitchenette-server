4352b3ef7a2db9e0eed2885603c5fa94a1149602
James Esguerra <jamesesguerra025@gmail.com>
2024-10-12 09:14:04 +0800

    add swagger open API UI generation

--- kitchenette-server/Program.cs
+++ kitchenette-server/Program.cs
@@ -1,8 +1,23 @@
+using Microsoft.OpenApi.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddProblemDetails();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+    x.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "kitchenette API",
+        Description = "An API for interacting with the kitchenette app",
+        Version = "1.0"
+    })
+);
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 if (!app.Environment.IsDevelopment())
 {
